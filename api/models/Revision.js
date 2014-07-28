e351e9f1785d10e402252a62a2b88e76a1a979cd
Takamura Soichi <takamura_s@ariel-networks.com>
2014-07-28 12:35:23 +0900

    fix: run difference upsert after revision upsert

--- api/models/Revision.js
+++ api/models/Revision.js
@@ -9,34 +9,40 @@
 module.exports = {
 
   attributes: {
-  	
+
   	/* e.g.
   	nickname: 'string'
   	*/
-    
+
   },
 
-  afterCreate: function (revision, next) {
-    Q.all(_.map(revision.differences, function (difference, key) {
-      var id = 'revision:' + revision.id + ':capture:' + key;
-      var doc = _.extend(_.clone(difference), {
-        id: id,
-        revision: revision.id,
-        capture: +key
-      });
-      return Difference.upsert(id, doc);
-    }))
-    .then(next)
-  },
+  afterCreate: afterUpsert,
+
+  afterUpdate: afterUpsert,
 
   upsert: function(id, document) {
-    return Revision.findOne(id)
+    return Difference.destroy({revision: id})
+    .then(function() {
+      return Revision.findOne(id);
+    })
     .then(function(doc) {
       return doc ?
           Revision.update(id, document).done(function() {}) :
           Revision.create(document).done(function() {});
-    })
+    });
   }
 
 };
 
+function afterUpsert(revision, next) {
+  Q.all(_.map(revision.differences, function(difference, key) {
+    var id = 'revision:' + revision.id + ':capture:' + key;
+    var doc = _.extend(_.clone(difference), {
+      id: id,
+      revision: revision.id,
+      capture: +key
+    });
+    return Difference.upsert(id, doc);
+  }))
+  .then(next);
+}
