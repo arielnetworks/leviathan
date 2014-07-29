bd6b91420aa3cc594455eb811e41e8eead48a6cb
Takamura Soichi <takamura_s@ariel-networks.com>
2014-07-29 21:41:42 +0900

    refac

--- api/services/leviathan.js
+++ api/services/leviathan.js
@@ -11,54 +11,62 @@ module.exports = {
    * @param {Array.<{expect_image, target_image}>} hints
    * @return {Promise}
    */
-  tidalwave: function(context) {
-    console.log('tidalwave begins');
-    return context.hints.reduce(function(p, hint) {
-      return p.then(function(differences) {
-        return request(hint).then(function(result) {
-          differences.push(result);
-          return differences;
-        })
-      });
-    }, Q([]))
-    .then(function(differences) {
-      console.log('tidalwave done');
-      context.differences = differences;
-      return context;
-    }).catch(function() {
-      console.log('xxx');
-    })
-  },
+  tidalwave: handleTidalwave,
 
-  store: function (context) {
-    console.log('storing begins');
-    return Difference.destroy({revision: context.id})
-    .then(function() {
-      return Q.all(_.map(context.differences, function (v, k) {
-        if (_.isEmpty(v)) return;
-        var id = Difference.generateId(context.id, k);
-        return Difference.upsertById(id, {
-          id: id,
-          result: v
-        })
-        .get('id')
-      })).then(_.compact)
-    })
-    .then(function(ids) {
-      context.differenceIds = ids;
-      delete context.differences;
-      return Revision.upsertById(context.id, context)
-    })
-    .then(function() {
-      return context;
-    })
-  }
+  store: handleStore
 
 };
 
 
 
 var socket = require('socket.io-client')('ws://10.0.2.90:5000');
+
+
+
+function handleTidalwave (context) {
+  console.log('tidalwave begins');
+  return context.hints.reduce(function(p, hint) {
+    return p.then(function(differences) {
+      return request(hint).then(function(result) {
+        differences.push(result);
+        return differences;
+      })
+    });
+  }, Q([]))
+  .then(function(differences) {
+    console.log('tidalwave done');
+    context.differences = differences;
+    return context;
+  }).catch(function() {
+    console.log('xxx');
+  })
+}
+
+
+
+function handleStore(context) {
+  console.log('storing begins');
+  return Difference.destroy({revision: context.id})
+  .then(function() {
+    return Q.all(_.map(context.differences, function (v, k) {
+      if (_.isEmpty(v)) return;
+      var id = Difference.generateId(context.id, k);
+      return Difference.upsertById(id, {
+        id: id,
+        result: v
+      })
+      .get('id')
+    })).then(_.compact)
+  })
+  .then(function(ids) {
+    context.differenceIds = ids;
+    delete context.differences;
+    return Revision.upsertById(context.id, context)
+  })
+  .then(function() {
+    return context;
+  })
+}
 
 
 
