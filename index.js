938543f2e3a93679db5590311759cf9a7004c881
Takamura Soichi <takamura_s@ariel-networks.com>
2014-11-28 15:10:54 +0900

    rm extra

--- index.js
+++ index.js
@@ -4,6 +4,5 @@ var path = require('path');
 
 require('./app').launch({
   baseImageDir: path.resolve(__dirname, 'test/fixture'),
-  relativeExpectedDir: 'expected',
   relativeTargetDirPrefix: 'revision'
 });
