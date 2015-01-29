d23550cac607118fc395421c95e938719e6b08fe
Takamura Soichi <takamura_s@ariel-networks.com>
2015-01-29 11:35:03 +0900

    minor

--- test/api.js
+++ test/api.js
@@ -50,28 +50,6 @@ describe('Application', function() {
       }, done);
     });
 
-    it('GET /api/captures' , function(done) {
-      request(app)
-      .get('/api/captures')
-      .expect(200)
-      .expect({
-        "captures": [
-          {
-            "capture": "9018988ae55e012e437aa24cbf9a400a",
-            "expectedRevision": ["1"],
-            "updatedAt": "1970-01-01T00:00:00.000Z",
-            "updatedBy": "system"
-          },
-          {
-            "capture": "db38f7f3f5d7d765f97e45d185066cc9",
-            "expectedRevision": ["1"],
-            "updatedAt": "1970-01-01T00:00:00.000Z",
-            "updatedBy": "system"
-          }
-        ]
-      }, done);
-    });
-
     it('GET /api/revisions/1 in' , function(done) {
       request(app)
       .get('/api/revisions/1')
@@ -149,6 +127,28 @@ describe('Application', function() {
         time: 0.1,
         updatedAt: '1970-01-01T00:00:00.000Z',
         vector: []
+      }, done);
+    });
+
+    it('GET /api/captures' , function(done) {
+      request(app)
+      .get('/api/captures')
+      .expect(200)
+      .expect({
+        "captures": [
+          {
+            "capture": "9018988ae55e012e437aa24cbf9a400a",
+            "expectedRevision": ["1"],
+            "updatedAt": "1970-01-01T00:00:00.000Z",
+            "updatedBy": "system"
+          },
+          {
+            "capture": "db38f7f3f5d7d765f97e45d185066cc9",
+            "expectedRevision": ["1"],
+            "updatedAt": "1970-01-01T00:00:00.000Z",
+            "updatedBy": "system"
+          }
+        ]
       }, done);
     });
 
