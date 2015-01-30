4d261e4ccc04eb9047fa43f6a0b7a5a83a5e172a
Takamura Soichi <takamura_s@ariel-networks.com>
2015-01-30 17:11:18 +0900

    multi webpage support done

--- ui/app/index.js
+++ ui/app/index.js
@@ -1,7 +1,7 @@
 
 var React = require('react');
 
-var BidimentionalTable = require('./components/BidimentionalTable');
+var BidimentionalTable = require('../components/BidimentionalTable');
 
 var AppIndex = React.createClass({
   render() {
